2fdee6cc6617db4a55a1c1e1000cf6cffdf977b4
DimmoX <86175879+DimmoX@users.noreply.github.com>
2024-08-24 00:53:11 -0400

    initial commit

---
+++ src/main/java/com/reserva/licencias_medicas/LicenciasMedicasApplication.java
@@ -0,0 +1,13 @@
+package com.reserva.licencias_medicas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LicenciasMedicasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LicenciasMedicasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reserva/licencias_medicas/LicenciasMedicasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reserva.licencias_medicas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LicenciasMedicasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
